5ecc7c4b6b7552338bba0ee79799454bd4cd29ad
littletomatodonkey <dazhiningsibuqu@163.com>
2020-04-06 12:32:57 +0000

    add tb

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'recommonmark',
-    #'sphinx_markdown_tables',
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
